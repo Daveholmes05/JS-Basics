838ded048ff5a9c30588341c0371557c7d726622
Dave Holmes <daveholmes05@gmail.com>
2016-05-07 12:12:29 -0600

    Finished

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -84,9 +84,16 @@ alert(newMyName("Dave"));
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
+  function outerFn() {
+  	return function () {
+  		return "Dave";
+  	}
+  }
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
+  var innerFn = outerFn();
 
 //Now invoke innerFn.
+innerFn();
